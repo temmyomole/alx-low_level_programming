7706f9fa818a257bf0c0f3fb39339b52db543c56
temmyomole <temmyomole19@gmail.com>
2022-10-13 14:36:00 -0700

    TEE

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - Entry
  * Return: Always o
  */
-int  main (void)
+int  main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
