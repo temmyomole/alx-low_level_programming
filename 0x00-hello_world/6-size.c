bc6293b2c9a7dfe9e3bdec4492137f6b839c0189
temmyomole <temmyomole19@gmail.com>
2022-10-13 23:07:38 -0700

    Tee

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)/n", size of (char));
+	printf("Size of a long int: 4 byte(s)/n", size of (long in));
+	printf("Size of a long long int: 8 byte(s)/n", size of (long long int));
+	printf("Size of a float: 4 byte(s)/n", size of (float));
+	printf("Size of a char: 1 byte(s)/n", size of (char));
+	printf("Size of an int: 4 byte(s)/n", size of (int));
+	printf("Size of a long int: 8 byte(s)/n", size of (long int));
+	printf("Size of a long long int: 8 byte(s)/n", size of (long long int));
+	printf("Size of a float: 4 byte(s)/n", size of (float));
+	return (0);
+}
