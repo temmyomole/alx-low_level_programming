dc012d03cba7f5ba7c5fdb42dfa271c085b8afea
temmyomole <temmyomole19@gmail.com>
2022-10-18 14:39:50 -0700

    tee

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void);
+{
+	char lca;
+
+	for (lca = 'a'; lca <= 'z'; lca++)
+		_putchar(lca);
+
+	_putchar('\n');
+}
