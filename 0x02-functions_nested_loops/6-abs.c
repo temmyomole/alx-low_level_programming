aac526a98ad25370cab431dca8a946ee02f353c1
temmyomole <temmyomole19@gmail.com>
2022-10-18 21:34:49 -0700

    tee

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * _abs - computes the absolute value of an interger
+ * @t: the integer to be computed
+ * Return: the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
