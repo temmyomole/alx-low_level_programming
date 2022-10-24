151066b83baa7266acdc78ba1a00eb7b5e657e8b
temmyomole <temmyomole19@gmail.com>
2022-10-24 04:01:10 -0700

    tee

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - A function that reset a value to 98
+ * @n: *n is an adress of an integer
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
