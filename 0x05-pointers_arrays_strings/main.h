8a27091685c9afa3f94a22de4e84980b38b962d1
temmyomole <temmyomole19@gmail.com>
2022-10-24 09:18:38 -0700

    tee

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,9 +10,10 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+int _putchar(char c)
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int _putchar(char c)
+
 
 
 
