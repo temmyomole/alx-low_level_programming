b08b03f6c0da6e15f2f401642f2f451311596f0b
temmyomole <temmyomole19@gmail.com>
2022-11-04 13:08:05 -0700

    tee

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: counts how many argument
+ * @argv:  store the variables
+ * Return: Always 0
+ */
+
+int main(int __attribute((__unused__)) argc, char *argv[])
+
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
