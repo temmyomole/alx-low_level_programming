7b8691e2ed786da3bea048e5c9d6fe13581a3f2c
temmyomole <temmyomole19@gmail.com>
2022-11-09 13:47:07 -0800

    Rufus

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,31 +10,23 @@
  *  Return: if failure return NULL
  *  else if if zero or negative for width and height null
  */
-
 int **alloc_grid(int width, int height)
-
 {
 	int **twoD;
-
 	int hgt_index, wid_index;
 
 	if (width <= 0 || height <= 0)
-
 		return (NULL);
-
 	twoD = malloc(sizeof(int *) * height);
 
 	if (twoD == NULL)
-
 		return (NULL);
 
 	for (hgt_index = 0; hgt_index < height; hgt_index++)
-
 	{
 		twoD[hgt_index] = malloc(sizeof(int) * width);
 
 		if (twoD[hgt_index] == NULL)
-
 		{
 			for (; hgt_index >= 0; hgt_index--)
 
@@ -43,14 +35,15 @@ int **alloc_grid(int width, int height)
 			free(twoD);
 
 			return (NULL);
+
 		}
+
 	}
 
 	for (hgt_index = 0; hgt_index < height; hgt_index++)
 
 	{
 		for (wid_index = 0; wid_index < width; wid_index++)
-
 			twoD[hgt_index][wid_index] = 0;
 	}
 
