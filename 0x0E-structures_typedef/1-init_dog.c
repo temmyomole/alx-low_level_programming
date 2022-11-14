1eddadafbcb270508456928ec1f72ff225136d4c
temmyomole <temmyomole19@gmail.com>
2022-11-14 15:46:05 -0800

    tee

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,17 +8,12 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
-
 {
 	if (d != NULL)
-
 	{
 		d->name = name;
-
 		d->age = age;
-
 		d->owner = owner;
 	}
 }
