b0980d55b0599d26c6b7243ffdb0d97a37606a29
temmyomole <temmyomole19@gmail.com>
2022-11-14 13:37:20 -0800

    tee

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints the name of the file of the program
+ * was compiled from, followed by a new line
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+
+	printf("%s\n", __FILE__);
+
+	return (0);
+
+}
+
+
