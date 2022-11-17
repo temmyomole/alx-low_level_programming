4252d45089d646986cb2a90aff7fb264fc7c1875
temmyomole <temmyomole19@gmail.com>
2022-11-17 11:26:17 -0800

    tee

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,36 @@
-#ifndef _CALC_H_
-#define _CALC_H_
-
-#include <stdlib.h>
-#include <stdio.h>
-
-
+#include "3-calc.h"
 
 /**
- * struct op - Struct op
- * @op: The operator
- * @f: The function associated
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
  */
-
-typedef struct op
-
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
+	int num1, num2;
+
 	char *op;
 
-	int (*f)(int a, int b);
-} op_t;
-
-int op_add(int a, int b);
-
-int op_sub(int a, int b);
-
-int op_mul(int a, int b);
-
-int op_div(int a, int b);
-
-int op_mod(int a, int b);
-
-int (*get_op_func(char *s))(int, int);
-
-
-
-#endif
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
+}
