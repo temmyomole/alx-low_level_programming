e205ebc586dfeecfe8edbbcc39195b39da5067be
temmyomole <temmyomole19@gmail.com>
2022-11-24 10:58:23 -0800

    tee

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,33 +1,30 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
 
-/*
- * File: lists.h
- * Auth: Omole Temiloluwa
- * Desc: Header file containing prototypes and definitions for all functions
- * and types written in the 0x11-singly_linked_lists directory.
- */
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
- * for ALX project
+ * for Holberton project
  */
-
 typedef struct list_s
 {
-	char *str;
+		char *str;
 
-	unsigned int len;
+			unsigned int len;
 
-	struct list_s *next;
-} list_t
-size_t print_list(const lis_t *h);
+				struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
